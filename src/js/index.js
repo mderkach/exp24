3eadb580b31399386f96a8827eb4c2ef2f0a1942
Skeev_ <derkach94@gmail.com>
2019-08-13 05:19:22 +0300

    documents page finish

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,12 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active')
   menu.classList.toggle('is-active')
 })
+
+//cards menu toggle
+let triggers = document.querySelectorAll('.cards__menu-trigger')
+
+triggers.forEach(function(trigger) {
+  trigger.addEventListener('click', function() {
+    this.parentElement.querySelector('.cards__menu-list').classList.toggle('is-active')
+  })
+})
