51e71b4ffa8701b5fabe8a0498cc69c503060346
Skeev_ <derkach94@gmail.com>
2019-09-17 21:17:03 +0300

    fonts

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -29,7 +29,7 @@ module.exports = {
     // module: `${PATHS.src}/your-module.js`,
   },
   output: {
-    filename: `${PATHS.assets}js/[name].[hash].js`,
+    filename: `${PATHS.assets}js/[name].js`,
     path: PATHS.dist,
     publicPath: '',
   },
@@ -50,7 +50,7 @@ module.exports = {
         options: {
           name: 'fonts/[name].[ext]',
           mimetype: 'application/font-woff',
-          publicPath: '/',
+          publicPath: '../',
         },
       },
       {
@@ -109,7 +109,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].[hash].css`,
+      filename: `${PATHS.assets}css/[name].css`,
     }),
     new CopyWebpackPlugin([
       { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
