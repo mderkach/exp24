189fd6a580dbb46321512b01cd265f02fe2c2ff2
mderkach <derkach94@gmail.com>
2020-04-11 12:51:15 +0300

    mchs + landing

--- src/js/slider__partners.js
+++ src/js/slider__partners.js
@@ -1,21 +1,25 @@
 import Swiper from 'swiper'
 
 var swiper = new Swiper('.slider-landing__partners', {
-  loop: true,
-  slidesPerView: 6,
-  spaceBetween: 20,
-  pagination: {
-    el: '.slider-landing__partners--pagination',
-    clickable: true,
-    bulletClass: 'slider-landing__partners--pagination--bullet',
-    bulletActiveClass: 'slider-landing__partners--pagination--bullet--active',
-  },
-  breakpoints: {
-    992: {
-      slidesPerView: 3,
-    },
-    576: {
-      slidesPerView: 1,
-    },
-  },
+	autoplay: {
+		delay: 3500,
+	},
+	loop: true,
+	initialSlide: -1,
+	slidesPerView: 6,
+	spaceBetween: 20,
+	pagination: {
+		el: '.slider-landing__partners--pagination',
+		clickable: true,
+		bulletClass: 'slider-landing__partners--pagination--bullet',
+		bulletActiveClass: 'slider-landing__partners--pagination--bullet--active',
+	},
+	breakpoints: {
+		992: {
+			slidesPerView: 3,
+		},
+		576: {
+			slidesPerView: 1,
+		},
+	},
 })
